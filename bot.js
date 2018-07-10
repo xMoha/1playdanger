87ae7d4d1b7ef2dcbc967da35baafb087a864dde
xMoha <40671043+xMoha@users.noreply.github.com>
2018-07-10 12:00:59 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -267,7 +267,7 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
     console.log(`in ${client.guilds.size} servers `);
     console.log(`[Codes] ${client.users.size}`);
-    client.user.setStatus("idle");
+    client.user.setStatus("DND");
 });
 
 
